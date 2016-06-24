725080f3e1a92f9487f1d42b2d80d4d2bf142e21
Saheb Roy <theioscracker@gmail.com>
2016-06-24 16:34:20 +0530

    Update EachNumber.m

--- EachNumber.m
+++ EachNumber.m
@@ -1,9 +1,7 @@
 //
 //  EachNumber.m
-//  ProbabilityApp
-//
 //  Created by Mac2 on 10/02/15.
-//  Copyright (c) 2015 webguru. All rights reserved.
+//  Copyright (c) 2015 OFTL. All rights reserved.
 //
 
 #import "EachNumber.h"
